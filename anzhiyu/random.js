621a9977a0c2e4628fc310bed43902200b739f09
Alllynnn <Alllynnn@users.noreply.github.com>
2024-12-21 04:58:07 +0000

    deploy: 20c89689140733f7b9fc4d607d2c0e88f4b0642a

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/14/github-actions-deploy/","2024/12/20/hello-world/","2024/01/07/my-first-post/"];function toRandomPost(){
+var posts=["2024/01/14/github-actions-deploy/","2024/12/21/hello-world/","2024/01/07/my-first-post/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
